040e02acf050d6e8c7a776e0032848aada593c3d
hansi <hansi@bihalu.de>
2024-05-01 12:48:44 +0200

    ~ podman or docker build

--- container/build.sh
+++ container/build.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-docker build --build-arg HOST=$1 --build-arg FORWARD_HOST=$2 -t localhost/ssf:3.0.0 .
+which podman
+[ $? -eq 0 ] && podman build --build-arg HOST=$1 --build-arg FORWARD_HOST=$2 -t localhost/ssf:3.0.0 .
+
+which docker
+[ $? -eq 0 ] && docker build --build-arg HOST=$1 --build-arg FORWARD_HOST=$2 -t localhost/ssf:3.0.0 .
+
 
 if [[ -f ssf.tar ]] ; then
   rm -f ssf.tar
 fi
 
-docker image save -o ssf.tar localhost/ssf:3.0.0
+which podman
+[ $? -eq 0 ] && podman image save -o ssf.tar localhost/ssf:3.0.0
+
+which docker
+[ $? -eq 0 ] && docker image save -o ssf.tar localhost/ssf:3.0.0
